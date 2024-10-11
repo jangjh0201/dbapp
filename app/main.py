46b58a87a85da39ed3721388fc43876b913f6ea2
jangjh0201 <jangjh0201@gmail.com>
2024-10-11 21:42:18 +0900

    Mod : 패키지 경로 재조정 및 로직 간소화

--- app/main.py
+++ app/main.py
@@ -1,39 +1,29 @@
 
-from database.database import SessionLocal
-from controller.auth_controller import router as auth_router
-from controller.manager_controller import router as manager_router
-from controller.user_controller import router as user_router
 import sys
 import os
+from utils.init_db import initialize_tables
+from router.v1 import home, beverage, item, auth
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
-from sqlalchemy.orm import Session
 
 # 프로젝트 루트 디렉토리를 sys.path에 추가
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
-
-app = FastAPI()
+app = FastAPI(
+    debug=True
+)
 
 # 정적 파일 설정
 app.mount("/static", StaticFiles(directory="app/resource/static"), name="static")
 
-# 유저, 매니저 및 인증 라우터 등록
-app.include_router(user_router)
-app.include_router(manager_router)
-app.include_router(auth_router)
+# 라우터 등록
+app.include_router(home.router)
+app.include_router(beverage.router)
+app.include_router(item.router)
+app.include_router(auth.router)
 
-# 데이터베이스 세션
-db = SessionLocal()
-
-
-# 테스트용 데이터 추가
-def add_test_data(db: Session):
-    return None
-
-
-# 테스트 데이터 추가
-add_test_data(db)
+# 테이블 및 데이터베이스 초기화
+initialize_tables()
 
 if __name__ == "__main__":
     import uvicorn
