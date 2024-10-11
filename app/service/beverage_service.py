282fc2245cd11a20c675e1b41e7a934152847d84
jangjh0201 <jangjh0201@gmail.com>
2024-10-11 21:30:16 +0900

    Mod : 서비스 클래스 생성, 함수 간소화

--- app/service/beverage_service.py
+++ app/service/beverage_service.py
@@ -1,74 +1,79 @@
 from sqlalchemy.orm import Session
-from models.models import Beverage
-from app.database.crud.beverage import create_beverage, read_beverage_by_id, read_all_beverages, update_beverage, delete_beverage_by_id
+from repository.beverage_repository import BeverageRepository
 
 
-def add_beverage(
-    beverage_name: str, beverage_quantity: int, db: Session
-):
+class BeverageService:
     """
-    음료 추가 함수
-    Args:
-        beverage_name: 음료 이름
-        beverage_quantity: 음료 수량
-        db: 데이터베이스 세션
-    Returns:
-        성공 여부
+    음료 서비스 클래스
     """
-    create_beverage(db, beverage_name, beverage_quantity)
-    return True
 
+    def __init__(self, db: Session):
+        self.beverage_repository = BeverageRepository(db)
 
-def get_all_beverages(db: Session):
-    """
-    모든 음료 정보 조회 함수
-    Args:
-        db: 데이터베이스 세션
-    Returns:
-        beverages: 모든 음료 정보 리스트
-    """
-    beverages = read_all_beverages(db)
-    return beverages
-
-
-def get_all_inventories(db: Session):
-    """
-    모든 음료 수량 정보 조회 함수
-    Args:
-        db: 데이터베이스 세션
-    Returns:
-        beverages: 모든 음료 수량 정보 딕셔너리
-    """
-    beverages = {bv.name: bv.quantity for bv in read_all_beverages(db)}
-    return beverages
-
-
-def remove_beverage(beverage_id: int, db: Session):
-    """
-    음료 삭제 함수
-    Args:
-        beverage_id: 음료 ID
-        db: 데이터베이스 세션
-    Returns:
-        성공 여부
-    """
-    delete_beverage_by_id(db, beverage_id)
-    return True
-
-
-def use_beverage(beverage_id: int, db: Session):
-    """
-    음료 수정 함수
-    Args:
-        beverage_id: 음료 ID
-        db: 데이터베이스 세션
-    Returns:
-        성공 여부
-    """
-    beverage = read_beverage_by_id(db, beverage_id)
-    if beverage.quantity == 0:
-        return False
-    else:
-        beverage.quantity -= 1
-        update_beverage(db, beverage)
+    def add_beverage(
+        self, beverage_name: str, beverage_quantity: int
+    ):
+        """
+        음료 추가 함수
+        Args:
+            beverage_name: 음료 이름
+            beverage_quantity: 음료 수량
+            db: 데이터베이스 세션
+        Returns:
+            성공 여부
+        """
+        self.beverage_repository.create_beverage(
+            beverage_name, beverage_quantity)
         return True
+
+    def get_all_beverages(self):
+        """
+        모든 음료 정보 조회 함수
+        Args:
+            db: 데이터베이스 세션
+        Returns:
+            beverages: 모든 음료 정보 리스트
+        """
+        beverages = self.beverage_repository.read_all_beverages()
+        return beverages
+
+    def get_all_inventories(self):
+        """
+        모든 음료 수량 정보 조회 함수
+        Args:
+            db: 데이터베이스 세션
+        Returns:
+            beverages: 모든 음료 수량 정보 딕셔너리
+        """
+        beverages = {
+            bv.name: bv.quantity for bv in self.beverage_repository.read_all_beverages(self)}
+        return beverages
+
+    def remove_beverage(self, beverage_id: int):
+        """
+        음료 삭제 함수
+        Args:
+            beverage_id: 음료 ID
+            db: 데이터베이스 세션
+        Returns:
+            성공 여부
+        """
+        self.beverage_repository.delete_beverage_by_id(beverage_id)
+        return True
+
+    def use_beverage(self, beverage_id: int):
+        """
+        음료 수정 함수
+        Args:
+            beverage_id: 음료 ID
+            db: 데이터베이스 세션
+        Returns:
+            성공 여부
+        """
+        beverage = self.beverage_repository.read_beverage_by_id(beverage_id)
+        if beverage.quantity == 0:
+            return False
+        else:
+            beverage.quantity -= 1
+            self.beverage_repository.update_beverage(beverage)
+            return True
--- app/service/item_service.py
+++ app/service/item_service.py
@@ -1,75 +1,74 @@
-from sqlalchemy.orm import Session
-from app.database.crud.item import create_item, read_all_items, read_item_by_id, delete_item_by_id, update_item
-from sqlalchemy import update
-from sqlalchemy import update
+from repository.item_repository import ItemRepository
 
 
-def add_item(
-    item_name: str, db: Session
-):
+class ItemService:
     """
-    아이템 추가 함수
-    Args:
-        item_name: 아이템 이름
-        storage: 아이템 보관 여부
-        db: 데이터베이스 세션
-    Returns:
-        성공 여부
+    아이템 서비스 클래스
     """
-    create_item(db, item_name, True)
-    return True
 
+    def __init__(self):
+        self.item_repository = ItemRepository()
 
-def get_all_items(db: Session):
-    """
-    모든 아이템 정보 조회 함수
-    Args:
-        db: 데이터베이스 세션
-    Returns:
-        items: 모든 아이템 정보 리스트
-    """
-    items = read_all_items(db)
-    return items
+    def add_item(self, item_name: str):
+        """
+        아이템 추가 함수
+        Args:
+            item_name: 아이템 이름
+            storage: 아이템 보관 여부
+            db: 데이터베이스 세션
+        Returns:
+            성공 여부
+        """
+        self.item_repository.create_item(item_name, True)
+        return True
 
+    def get_all_items(self):
+        """
+        모든 아이템 정보 조회 함수
+        Args:
+            db: 데이터베이스 세션
+        Returns:
+            items: 모든 아이템 정보 리스트
+        """
+        items = self.item_repository.read_all_items()
+        return items
 
-def remove_item(item_id: int, db: Session):
-    """
-    아이템 삭제 함수
-    Args:
-        item_id: 아이템 ID
-        db: 데이터베이스 세션
-    Returns:
-        성공 여부
-    """
-    delete_item_by_id(db, item_id)
-    return True
+    def remove_item(self, item_id: int):
+        """
+        아이템 삭제 함수
+        Args:
+            item_id: 아이템 ID
+            db: 데이터베이스 세션
+        Returns:
+            성공 여부
+        """
+        self.item_repository.delete_item_by_id(item_id)
+        return True
 
+    def edit_item_name(self, item_id: int, name: str):
+        """
+        아이템 이름 수정 함수
+        Args:
+            item_id: 아이템 ID
+            db: 데이터베이스 세션
+        Returns:
+            성공 여부
+        """
+        self.item_repository.update_item(item_id, name, None)
+        return True
 
-def edit_item_name(item_id: int, name: str, db: Session):
-    """
-    아이템 이름 수정 함수
-    Args:
-        item_id: 아이템 ID
-        db: 데이터베이스 세션
-    Returns:
-        성공 여부
-    """
-    update_item(db, item_id, name, None)
-    return True
-
-
-def change_item_storage(item_id: int, request: bool, db: Session):
-    """
-    아이템 상태 변경 함수
-    Args:
-        item_id: 아이템 ID
-        db: 데이터베이스 세션
-    Returns:
-        성공 여부
-    """
-    if request == True:
-        update(db, item_id, None, False)
-    elif request == False:
-        update(db, item_id, None, True)
-    else:
-        return False
+    def change_item_storage(self, item_id: int, request: bool):
+        """
+        아이템 상태 변경 함수
+        Args:
+            item_id: 아이템 ID
+            db: 데이터베이스 세션
+        Returns:
+            성공 여부
+        """
+        if request == True:
+            self.item_repository.update_item(item_id, None, False)
+        elif request == False:
+            self.item_repository.update_item(item_id, None, True)
+        else:
+            return False
